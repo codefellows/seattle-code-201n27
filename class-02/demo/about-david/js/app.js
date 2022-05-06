23267f0fb44b256c42c032844ea9f7447eb6e644
David Souther <davidsouther+github@gmail.com>
2022-05-05 20:43:01 -0700

    Yes/No Questions

--- class-02/demo/about-david/js/app.js
+++ class-02/demo/about-david/js/app.js
@@ -1,44 +1,60 @@
 "use strict";
 
-let answer1 = prompt("What is the name of one of my pets?");
+// Mandy
+// let userName = prompt("What is your name?");
+// alert("Welcome to my About Me page, " + userName + ".");
+// console.log(userName);
 
-// console.log(answer1);
+// Alejandro
+// let proceed = prompt("Do you want to proceed?");
+// if (proceed === "yes") {
+//   alert("Great!");
+// } else {
+//   alert("Sorry to hear that.");
+// }
 
-if (true) {
-  console.log("In a condition that executes always.");
-}
-
-if (false) {
-  console.log("In a condition that executes never!");
-}
-
-if (Math.random() > 0.5) {
-  console.log("This condition maybe runs!");
-}
-
-answer1 = answer1.toLowerCase();
-if (answer1 == "oliver") {
-  console.log("Oliver is my corgi!");
-} else if (answer1 == "pippin" || answer1 == "greylien") {
-  console.log(answer1 + " is my cat!");
-} else if (answer1 == "greylien") {
-  console.log(
-    "Greylien is my interdimensional space monster inhabiting a cat's body"
-  );
+// Lauren
+let answer1 = prompt(
+  "Have I visited all three WA national parks?"
+)?.toLowerCase();
+if (answer1 === "yes" || answer1 === "y") {
+  alert("Yes! I have been to Olymic, Rainier, and North Cascades");
+} else if (answer1 === "no" || answer1 === "n") {
+  alert("But they're so close!");
 } else {
-  console.log(answer1 + " is not a pet of mine.");
+  alert("Please answer with a (y)es or (n)o");
 }
 
-switch (answer1) {
-  case "Oliver":
-    console.log("Oliver says waroo");
-    break;
-  case "Pippin":
-    console.log("Pippin says meewww");
-    break;
-  case "Greylien":
-    console.log("Greylien says mrhh");
-    break;
-  default:
-    console.log(answer1 + " says 'Adopt me please!'");
-}
+// Gabriella
+// let farm = prompt("Do I want cows on my farm?")?.toLowerCase();
+// if (farm === "yes" || farm === "y") {
+//   alert("Yes, I want brown cows on my farm!");
+// } else if (farm === "no" || farm === "n") {
+//   alert("Why would I not want brown cows on my farm?");
+// } else {
+//   alert("Please answer with a (y)es or (n)o");
+// }
+
+// Dan
+// let smoothie = prompt("Is my favorite smoothie flavor blackberry?");
+// switch (smoothie) {
+//   case "yes":
+//     alert("That is indeed my favorite smoothie");
+//     break;
+//   case "no":
+//     alert("That is incorrect, blueberry is my favorite smoothie.");
+//     break;
+// }
+
+// Mario
+// let insects = prompt("Do I love insects?");
+// switch (insects?.toLowerCase()) {
+//   case "n":
+//   case "no":
+//     alert("You are correct, I hate bugs!");
+//     break;
+//   case "y":
+//   case "yes":
+//     alert("That is incorrect, I do not like bugs. They should stay outside.");
+//     break;
+// }
